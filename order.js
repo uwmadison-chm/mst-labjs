772b11e2f6a4dd648c9f5a3e9e2c0a430db36093
Dan Fitch <dgfitch@gmail.com>
2019-08-12 10:44:35 -0500

    Stable rng

--- order.js
+++ order.js
@@ -1,10 +1,23 @@
 'use strict';
 
+var seedrandom = require('seedrandom');
+
 module.exports = class OrderFiller {
-  constructor(order, lureDifficulty) {
+  constructor(seedOrRng, order, lureDifficulty) {
+    if (typeof seedOrRng === "string" || typeof seedOrRng === "number") {
+      this.rng = seedrandom(seedOrRng);
+    } else {
+      this.rng = seedOrRng;
+    }
     this.order = order;
     this.lureDifficulty = lureDifficulty;
     this.availableStimuli = [...Array(193).keys()].slice(1);
+  }
+
+  popStimuli() {
+    var index = this.availableStimuli.length * this.rng() | 0;
+    var value = this.availableStimuli.splice(index, 1);
+    return value[0];
   }
 }
 
--- test/order.js
+++ test/order.js
@@ -1,15 +1,17 @@
 var assert = require('chai').assert;
+
+var seedrandom = require('seedrandom');
 var OrderFiller = require('../order');
 var o;
 
 describe('OrderFiller', function() {
   beforeEach(function() {
-    o = new OrderFiller([], {});
+    o = new OrderFiller('test', [], {});
   });
 
   describe('should initialize correctly', function() {
     it('loads stim orders', function() {
-      o = new OrderFiller([
+      o = new OrderFiller('new_seed', [
         {"stim_bin_index":42,"trial_type":"repeat","repetition":"a","lag":65,"trial_number":1}
       ], {});
       assert.equal(o.order[0].stim_bin_index, 42);
@@ -27,9 +29,29 @@ describe('OrderFiller', function() {
     });
   });
 
+  describe('rng is stable', function() {
+    it('generates same number', function() {
+      assert.equal(o.rng(), 0.8722025543160253);
+    });
+
+    it('accepts seed and still generates a stable number', function() {
+      o = new OrderFiller('eclosion', [], {});
+      assert.equal(o.rng(), 0.619874173930009);
+    });
+
+    it('can use a passed-in rng', function() {
+      var r = seedrandom('eclosion');
+      o = new OrderFiller(r, [], {});
+      assert.equal(o.rng(), 0.619874173930009);
+    });
+  });
+
   describe('should insert stimuli correctly', function() {
     it('pops and removes stimuli', function() {
-      assert.equal(o.availableStimuli.length, 192);
+      var stimuli = o.popStimuli();
+      assert.equal(o.availableStimuli.length, 191);
+      assert.isAtLeast(stimuli, 1);
+      assert.isAtMost(stimuli, 192);
     });
   });
 });
